5c15837d8fcd88e4b044e466fe00553782302748
Rabia Alhaffar <steria773@gmail.com>
2020-11-16 17:20:33 +0200

    Version v0.2 released! + Web build works!

--- app/main.rb
+++ app/main.rb
@@ -1,19 +1,21 @@
 # Written by Rabia Alhaffar in 14/November/2020
 # Stacking game for teeny-tiny game jam
 # I got cutting block logic from following repo: https://github.com/tma02/stack
-# Background from freepik.com: https://www.freepik.com/free-vector/sweet-pastries-chalkboard_1529234.htm
+# Background: http://getwallpapers.com/collection/cute-cupcake-backgrounds
 # Stacking sound: https://freesound.org/people/kemitix/sounds/41869
 
 def tick args
   # Load variables
   setup args
   
-  # Check and render current scene
-  if args.state.scene == 0
-    game args
-  elsif args.state.scene == 1
-    lose args
-  end
+  # Draw game background
+  args.outputs.primitives << {
+    x: 0,
+	y: 0,
+	w: 1280,
+	h: 720,
+	path: "sprites/background.jpg"
+  }
   
   # Draw FPS
   args.outputs.primitives << {
@@ -26,6 +28,13 @@ def tick args
 	b: 0,
 	a: 255,
   }
+  
+  # Check and render current scene
+  if args.state.scene == 0
+    game args
+  elsif args.state.scene == 1
+    lose args
+  end
   
   # ESC: Exit game
   if args.inputs.keyboard.escape
@@ -42,6 +51,7 @@ end
 def setup args
   args.state.scene  ||= 0   # Current scene
   args.state.score  ||= 0   # Score
+  args.state.speed  ||= 0.5 # Speed to increase when stacking each time
   args.state.blocks ||= []  # Blocks stacked
   
   # Load base and movement block
@@ -56,11 +66,11 @@ def setup args
   
   args.state.current_block ||= {
     speed: 6,
-	x: 340,
-	y: 100,
-	w: 600,
-	h: 100, 
-	path: "sprites/1.png"
+	x: args.state.start_block.x,
+	y: args.state.start_block.y + args.state.start_block.h,
+	w: args.state.start_block.w,
+	h: args.state.start_block.h, 
+	path: "sprites/#{rand(4) + 1}.png"
   }
 end
 
@@ -68,16 +78,7 @@ def game args
   # Move block by speed
   args.state.current_block.x += args.state.current_block[:speed]
   
-  # Draw background, Score, and blocks...
-  args.outputs.primitives << {
-    x: 0,
-	y: 0,
-	w: 1280,
-	h: 720,
-	path: "sprites/background.jpg",
-	a: 230
-  }
-  
+  # Draw Score and blocks...  
   args.outputs.primitives << {
     x: 600,
 	y: 10.from_top,
@@ -110,48 +111,48 @@ def game args
     end
 	
 	# If smaller than or equal to 0, Game over!
+	# Else, Continue game
     if (new_width <= 0)
-      new_width = 0
 	  args.state.scene = 1
-	end
-	
-	# Push block
-	args.state.blocks.push({
-	  x: new_x,
-	  y: args.state.current_block.y,
-	  w: new_width,
-	  h: 100,
-	  path: args.state.current_block.path
-    })
-	
-	# Setting width of movement block with last block stacked, With random cake image...
-	args.state.current_block.w = args.state.blocks[args.state.blocks.length() - 1].w
-	args.state.current_block.path = "sprites/#{rand(4) + 1}.png"
-	
-	# Increase score and play sound
-	args.state.score += 1
-	args.outputs.sounds << "audio/stack.wav"
-	
-	# If stack more than 4, Move game objects to up (This works like a 2D camera...)
-    # Else, Keep with increasing y of movement block
-	if args.state.score > 3
-	  args.state.blocks.each do |r|
-        r[:y] -= 100
-      end
-	  args.state.start_block.y -= 100
 	else
-	  args.state.current_block.y += 100
-	end
+	  # Increase score and play sound
+ 	  args.state.score += 1
+	  args.outputs.sounds << "audio/stack.wav"
 	
-	# Increase speed by 0.5, And reverse movement direction of movement block
-	args.state.current_block[:speed] = -(args.state.current_block[:speed].abs + 0.5)
+	  # Push block
+	  args.state.blocks.push({
+	    x: new_x,
+	    y: args.state.current_block.y,
+	    w: new_width,
+	    h: args.state.current_block.h,
+	    path: args.state.current_block.path
+      })
+	
+	  # Setting width of movement block with last block stacked, With random cake image...
+	  args.state.current_block.w = args.state.blocks[args.state.blocks.length() - 1].w
+	  args.state.current_block.h = args.state.blocks[args.state.blocks.length() - 1].h
+	  args.state.current_block.path = "sprites/#{rand(4) + 1}.png"
+	
+	  # If stack more than 4, Move game objects to up (This works like a 2D camera...)
+      # Else, Keep with increasing y of movement block
+	  if args.state.score > 3
+	    args.state.blocks.each do |r|
+          r[:y] -= args.state.current_block.h
+        end
+	    args.state.start_block.y -= args.state.start_block.h
+	  else
+	    args.state.current_block.y += args.state.current_block.h
+	  end
+	
+	  # Increase speed by 0.5, And reverse movement direction of movement block
+	  args.state.current_block[:speed] = -(args.state.current_block[:speed].abs + args.state.speed)
+    end
   end
   
   # Reverse movement direction of movement block if hit with left or right side of game window...
   if (args.state.current_block.x + args.state.current_block.w >= 1280) || (args.state.current_block.x <= 0)
     args.state.current_block[:speed] = -args.state.current_block[:speed]
   end
-  
 end
 
 def lose args
@@ -175,7 +176,7 @@ def lose args
   }.label
   
   # Restart game if user click...
-  if args.inputs.mouse.click
+  if args.inputs.keyboard.key_up.space || args.inputs.mouse.click
     $gtk.reset seed: (Time.now.to_f * 100).to_i
   end
 end
